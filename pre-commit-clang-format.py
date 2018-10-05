858944d8fbbc9ceabeb40d0adc443cfa2089c927
Leedehai <hhlisu@yahoo.com>
2018-10-04 22:45:57 -0700

    Fix the bug of not restaging after clang-formatting

--- pre-commit-clang-format.py
+++ pre-commit-clang-format.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-# This script basically does git-clang-format's work, but I'd like to keep dependency small
-# so I wrote my own
+# Basically does git-clang-format's work, but I'd like to keep dependency small.
 
 import os, sys, subprocess
 import mygitutils, shouldvisit # my own
@@ -8,14 +7,22 @@ import mygitutils, shouldvisit # my own
 THIS_DIR = os.path.dirname(__file__)
 FORMAT_UTIL = "clang-format"
 
-# get tracked modified files (staged only)
-files = mygitutils.get_staged_modified_in_tracked()
-
+files = mygitutils.get_staged_modified_in_tracked() # modified files (staged only)
+if len(files) == 0:
+    sys.exit(0)
 for path in files:
     if shouldvisit.should_visit(path):
         print("\tvisit %s" % path)
-        subprocess.call(" ".join([FORMAT_UTIL, "-i", path]), shell=True) # -i: modify in-place
+        # -i: modify in-place
+        if 0 != subprocess.call(" ".join([FORMAT_UTIL, "-i", path]), shell=True):
+            print("[Error] error: %s -i %s" % (FORMAT_UTIL, path))
+            sys.exit(1)
     else:
         print("\tskip  %s" % path)
 
-sys.exit(0)
+# restage and exit
+if 0 == subprocess.call(" ".join(["git add"] + files), shell=True):
+    sys.exit(0)
+else:
+    print("[Error] error at re-running 'git add' after formatting")
+    sys.exit(1)
